51ffe36071d5c06b49a0136908cdd86e89aa5f72
Emir <emiratesbp@gmail.com>
2015-03-30 11:46:43 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="536 Tamiami Trail North"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples, Fl 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2392628181;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slide"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
